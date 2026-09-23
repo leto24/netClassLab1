5214eed5931b31e22f7a6b830affc9a74a413acb
leto24 <carlbenn24@gmail.com>
2026-09-23 15:27:37 -0500

    Update assign2.py

--- assign2.py
+++ assign2.py
@@ -13,15 +13,15 @@ def bandwidth(f_min, f_max):
 
 # Task 4: Return the attenuation (dB) given input and output power
 def attenuation_db(p_in, p_out):
-    return 10 * math.log10(p_in/p_out)
+    return 10 * math.log(p_in/p_out,10)
 
 # Task 5: Return the Nyquist capacity (bps) for bandwidth B and M signal levels
 def nyquist_capacity(B, M):
-    return 2 * B * math.log2(M)
+    return 2 * B * math.log(M,2)
 # Task 6: Return the Shannon capacity (bps) for bandwidth B and SNR in dB
 def shannon_capacity(B, snr_db):
     snr = 10 ** (snr_db / 10)
-    return B * math.log2(1 + snr)
+    return B * math.log(1 + snr,2)
 
 # ----- Do not change anything below this line -----
 print("Task 1: Period of 2 Hz signal =", period(2), "seconds")
